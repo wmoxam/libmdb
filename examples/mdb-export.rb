00b65b9da4c814723579b40651dd290ac7a8aad0
Wesley Moxam <wesley.moxam@gmail.com>
2012-10-02 16:32:05 -0400

    Adds a port of mdb-export

---
+++ examples/mdb-export.rb
@@ -0,0 +1,212 @@
+# Ruby port of mdb-export
+# https://github.com/brianb/mdbtools/blob/master/src/util/mdb-export.c
+
+require 'getoptlong'
+require 'rubygems'
+require '../lib/mdb'
+
+MDB::BIND_SIZE = 200000
+
+def is_quote_type?(x)
+  [:text, :ole, :memo, :datetime, :binary, :repid].include?(x)
+end
+
+def is_binary_type?(x)
+  [:ole, :binary, :repid].include?(x)
+end
+
+def print_col(col_val, quote_text, col_type, quote_char, escape_char)
+  value = col_val[:value].to_s
+
+	if quote_text && is_quote_type?(col_type)
+		print quote_char
+    value.gsub!(escape_char, escape_char * 2) unless escape_char.nil?
+    value.gsub!(quote_char, escape_char.to_s + quote_char.to_s) unless quote_char.nil?
+    print value
+		print quote_char
+	else
+		print col_val[:value].to_s
+  end
+end
+
+def escapes(s)
+  encode = false
+  encoded = ""
+
+  s.to_s.split("").each do |char|
+		if (encode)
+			case char
+			when 'n': encoded << '\n'; break;
+			when 't': encoded << '\t'; break;
+			when 'r': encoded << '\r'; break;
+      else
+        encoded << "\\#{char}"; break;
+      end
+			encode = false
+		elsif char == '\\'
+			encode = true
+		else
+			encoded << s
+		end
+	end
+  encoded
+end
+
+opts = GetoptLong.new(
+  [ '-H', GetoptLong::NO_ARGUMENT ],
+  [ '-d', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '-R', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '-I', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '-D', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '-q', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '-X', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '-N', GetoptLong::REQUIRED_ARGUMENT ]
+)
+
+@header_row = true
+@quote_text = true
+@quote_char = "\""
+@delimiter = ","
+@row_delimiter = "\n"
+@escape_char = nil
+@namespace = nil
+@insert_dialect = nil
+
+opts.each do |opt, arg|
+  case opt
+  when '-H'
+    @header_row = false
+    break
+	when '-Q'
+		@quote_text = false
+		break
+	when '-q'
+		@quote_char = arg
+		break
+	when '-d'
+		@delimiter = escapes(arg)
+		break
+	when '-R':
+		@row_delimiter = escapes(arg)
+		break
+	when '-I'
+		@insert_dialect = arg
+		@header_row = false
+		break
+	when '-D'
+		MDB::mdb_set_date_fmt(arg)
+		break
+	when '-X'
+		@escape_char = arg
+		break
+	when '-N'
+		@namespace = arg
+		break
+  else
+		break
+  end
+end
+
+if ARGV.length < 2
+	STDERR.puts "Usage: #{argv[0]} [options] <file> <table>"
+	STDERR.puts "where options are:"
+	STDERR.puts "  -H             supress header row"
+	STDERR.puts "  -Q             don't wrap text-like fields in quotes"
+	STDERR.puts "  -d <delimiter> specify a column delimiter"
+	STDERR.puts "  -R <delimiter> specify a row delimiter"
+	STDERR.puts "  -I <backend>   INSERT statements (instead of CSV)"
+	STDERR.puts "  -D <format>    set the date format (see strftime(3) for details)"
+	STDERR.puts "  -q <char>      Use <char> to wrap text-like fields. Default is \"."
+	STDERR.puts "  -X <char>      Use <char> to escape quoted characters within a field. Default is doubling."
+	STDERR.puts "  -N <namespace> Prefix identifiers with namespace"
+	exit(1)
+end	
+
+@file_name = ARGV[0]
+@table_name = ARGV[1]
+
+@mdb = MDB::mdb_open(@file_name, :no_flags)
+
+exit(1) if !@mdb
+
+if @insert_dialect
+	if MDB::mdb_set_default_backend(@mdb, @insert_dialect) == 0
+		STDERR.puts "Invalid backend type"
+		exit(1)
+	end
+end
+
+@table = MDB::mdb_read_table_by_name(@mdb, @table_name, :table)
+
+if !@table
+	STDERR.puts "Error: Table #{@table_name} does not exist in this database."
+	MDB::mdb_close(@mdb)
+	exit(1)
+end
+
+MDB::mdb_read_columns(@table)
+MDB::mdb_rewind_table(@table)
+
+@number_of_columns = @table[:num_cols].to_i
+@bound_values = Array.new(@number_of_columns)
+
+0.upto(@number_of_columns - 1) do |j|
+  value = MDB::BoundValue.new
+  value[:value].to_ptr.put_string(0, "")
+
+  length = MDB::BoundLength.new
+  length[:len] = 0
+
+  @bound_values[j] = [value, length]
+
+  MDB::mdb_bind_column @table, j+1, @bound_values[j][0].to_ptr, @bound_values[j][1].to_ptr
+end
+
+column_pointers = MDB::PtrArray.new @table[:columns]
+
+@columns = column_pointers[:pdata].read_array_of_type(:pointer, :read_pointer, column_pointers[:len]).map do |pointer|
+  pointer.null? ? nil : MDB::MdbColumn.new(pointer)
+end.compact
+
+if @header_row
+  @columns.each_with_index do |column,i|
+		print @delimiter if i > 0
+		print column[:name]
+  end
+	puts ""
+end
+
+while(MDB::mdb_fetch_row(@table) != 0) do
+	if @insert_dialect
+	  quoted_name = @mdb[:default_backend][:quote_schema_name].call(@namespace, @table_name)
+		print "INSERT INTO #{quoted_name} ("
+    @columns.each_with_index do |column,j|
+			print ', '	if j > 0
+			quoted_name = @mdb[:default_backend][:quote_schema_name].call(nil, column[:name])
+			print quoted_name
+		end
+		print ") VALUES ("
+	end
+
+  @columns.each_with_index do |column,j|
+		print @delimiter if j > 0
+		if @bound_values[j][1][:len] == 0
+		  print "NULL" if @insert_dialect
+		else
+      length = value = nil
+			if column[:col_type] == :ole
+        length = FFI::MemoryPointer.new(:int)
+				value = MDB::mdb_ole_read_full(@mdb, column, length)
+		  else
+				value = @bound_values[j][0]
+				length = @bound_values[j][1]
+			end
+			print_col(value, @quote_text, column[:col_type], @quote_char, @escape_char)
+		end
+	end
+  print ");" if @insert_dialect
+  print @row_delimiter
+end
+
+MDB::mdb_free_tabledef(@table)
+MDB::mdb_close(@mdb)
--- lib/mdb.rb
+++ lib/mdb.rb
@@ -235,12 +235,155 @@ module MDB
   end
 
 
+  # file
+  #extern ssize_t mdb_read_pg(MdbHandle *mdb, unsigned long pg);
+  #extern ssize_t mdb_read_alt_pg(MdbHandle *mdb, unsigned long pg);
+  #extern unsigned char mdb_get_byte(void *buf, int offset);
+  #extern int    mdb_get_int16(void *buf, int offset);
+  #extern long   mdb_get_int32(void *buf, int offset);
+  #extern long   mdb_get_int32_msb(void *buf, int offset);
+  #extern float  mdb_get_single(void *buf, int offset);
+  #extern double mdb_get_double(void *buf, int offset);
+  #extern unsigned char mdb_pg_get_byte(MdbHandle *mdb, int offset);
+  #extern int    mdb_pg_get_int16(MdbHandle *mdb, int offset);
+  #extern long   mdb_pg_get_int32(MdbHandle *mdb, int offset);
+  #extern float  mdb_pg_get_single(MdbHandle *mdb, int offset);
+  #extern double mdb_pg_get_double(MdbHandle *mdb, int offset);
   attach_function :mdb_open, [:string, :file_flags], MdbHandle.by_ref
-  attach_function :mdb_read_table_by_name, [MdbHandle, :string, :obj_type], MdbTableDef.by_ref
-  attach_function :mdb_read_columns, [MdbTableDef], :pointer
-  attach_function :mdb_rewind_table, [MdbTableDef], :int
-  attach_function :mdb_bind_column, [MdbTableDef, :int, :pointer, :pointer], :void
-  attach_function :mdb_fetch_row, [MdbTableDef], :int
-  attach_function :mdb_free_tabledef, [MdbTableDef], :void
   attach_function :mdb_close, [MdbTableDef], :void
+  #extern MdbHandle *mdb_clone_handle(MdbHandle *mdb);
+  #extern void mdb_swap_pgbuf(MdbHandle *mdb);
+
+  # catalog
+  #extern void mdb_free_catalog(MdbHandle *mdb);
+  #extern GPtrArray *mdb_read_catalog(MdbHandle *mdb, int obj_type);
+  #MdbCatalogEntry *mdb_get_catalogentry_by_name(MdbHandle *mdb, const gchar* name);
+  #extern void mdb_dump_catalog(MdbHandle *mdb, int obj_type);
+  #extern char *mdb_get_objtype_string(int obj_type);
+
+  # table
+  #extern MdbTableDef *mdb_alloc_tabledef(MdbCatalogEntry *entry);
+  attach_function :mdb_free_tabledef, [MdbTableDef], :void
+  #extern MdbTableDef *mdb_read_table(MdbCatalogEntry *entry);
+  attach_function :mdb_read_table_by_name, [MdbHandle, :string, :obj_type], MdbTableDef.by_ref
+  #extern void mdb_append_column(GPtrArray *columns, MdbColumn *in_col);
+  #extern void mdb_free_columns(GPtrArray *columns);
+  attach_function :mdb_read_columns, [MdbTableDef], :pointer
+  #extern void mdb_table_dump(MdbCatalogEntry *entry);
+  #extern guint8 read_pg_if_8(MdbHandle *mdb, int *cur_pos);
+  #extern guint16 read_pg_if_16(MdbHandle *mdb, int *cur_pos);
+  #extern guint32 read_pg_if_32(MdbHandle *mdb, int *cur_pos);
+  #extern void *read_pg_if_n(MdbHandle *mdb, void *buf, int *cur_pos, size_t len);
+  #extern int mdb_is_user_table(MdbCatalogEntry *entry);
+  #extern int mdb_is_system_table(MdbCatalogEntry *entry);
+  #extern const char *mdb_table_get_prop(const MdbTableDef *table, const gchar *key);
+  #extern const char *mdb_col_get_prop(const MdbColumn *col, const gchar *key);
+
+  #/* data.c */
+  #extern int mdb_bind_column_by_name(MdbTableDef *table, gchar *col_name, void *bind_ptr, int *len_ptr);
+  #extern void mdb_data_dump(MdbTableDef *table);
+  #extern void mdb_date_to_tm(double td, struct tm *t);
+  attach_function :mdb_bind_column, [MdbTableDef, :int, :pointer, :pointer], :void
+  attach_function :mdb_rewind_table, [MdbTableDef], :int
+  attach_function :mdb_fetch_row, [MdbTableDef], :int
+  #extern int mdb_is_fixed_col(MdbColumn *col);
+  #extern char *mdb_col_to_string(MdbHandle *mdb, void *buf, int start, int datatype, int size);
+  #extern int mdb_find_pg_row(MdbHandle *mdb, int pg_row, void **buf, int *off, size_t *len);
+  #extern int mdb_find_row(MdbHandle *mdb, int row, int *start, size_t *len);
+  #extern int mdb_find_end_of_row(MdbHandle *mdb, int row);
+  #extern int mdb_col_fixed_size(MdbColumn *col);
+  #extern int mdb_col_disp_size(MdbColumn *col);
+  #extern size_t mdb_ole_read_next(MdbHandle *mdb, MdbColumn *col, void *ole_ptr);
+  #extern size_t mdb_ole_read(MdbHandle *mdb, MdbColumn *col, void *ole_ptr, int chunk_size);
+  attach_function :mdb_ole_read_full, [MdbHandle, MdbColumn, :pointer], :string
+  attach_function :mdb_set_date_fmt, [:string], :void
+  #extern int mdb_read_row(MdbTableDef *table, unsigned int row);
+
+  #/* dump.c */
+  #extern void mdb_buffer_dump(const void *buf, int start, size_t len);
+
+  #/* backend.c */
+  #extern char* __attribute__((deprecated)) mdb_get_coltype_string(MdbBackend *backend, int col_type);
+  #extern int __attribute__((deprecated)) mdb_coltype_takes_length(MdbBackend *backend, int col_type);
+  #extern const MdbBackendType* mdb_get_colbacktype(const MdbColumn *col);
+  #extern const char* mdb_get_colbacktype_string(const MdbColumn *col);
+  #extern int mdb_colbacktype_takes_length(const MdbColumn *col);
+  #extern void __attribute__((deprecated)) mdb_init_backends();
+  #extern void mdb_register_backend(char *backend_name, guint32 capabilities, MdbBackendType *backend_type, MdbBackendType *type_shortdate, MdbBackendType *type_autonum, const char *short_now, const char *long_now, const char *charset_statement, const char *drop_statement, const char *constaint_not_empty_statement, const char *column_comment_statement, const char *table_comment_statement, gchar* (*quote_schema_name)(const gchar*, const gchar*));
+  #extern void __attribute__((deprecated)) mdb_remove_backends();
+  attach_function :mdb_set_default_backend, [MdbHandle, :string], :int
+  #extern void mdb_print_schema(MdbHandle *mdb, FILE *outfile, char *tabname, char *dbnamespace, guint32 export_options);
+
+  #/* sargs.c */
+  #extern int mdb_test_sargs(MdbTableDef *table, MdbField *fields, int num_fields);
+  #extern int mdb_test_sarg(MdbHandle *mdb, MdbColumn *col, MdbSargNode *node, MdbField *field);
+  #extern void mdb_sql_walk_tree(MdbSargNode *node, MdbSargTreeFunc func, gpointer data);
+  #extern int mdb_find_indexable_sargs(MdbSargNode *node, gpointer data);
+  #extern int mdb_add_sarg_by_name(MdbTableDef *table, char *colname, MdbSarg *in_sarg);
+  #extern int mdb_test_string(MdbSargNode *node, char *s);
+  #extern int mdb_test_int(MdbSargNode *node, gint32 i);
+  #extern int mdb_add_sarg(MdbColumn *col, MdbSarg *in_sarg);
+
+  #/* index.c */
+  #extern GPtrArray *mdb_read_indices(MdbTableDef *table);
+  #extern void mdb_index_dump(MdbTableDef *table, MdbIndex *idx);
+  #extern void mdb_index_scan_free(MdbTableDef *table);
+  #extern int mdb_index_find_next_on_page(MdbHandle *mdb, MdbIndexPage *ipg);
+  #extern int mdb_index_find_next(MdbHandle *mdb, MdbIndex *idx, MdbIndexChain *chain, guint32 *pg, guint16 *row);
+  #extern void mdb_index_hash_text(char *text, char *hash);
+  #extern void mdb_index_scan_init(MdbHandle *mdb, MdbTableDef *table);
+  #extern int mdb_index_find_row(MdbHandle *mdb, MdbIndex *idx, MdbIndexChain *chain, guint32 pg, guint16 row);
+  #extern void mdb_index_swap_n(unsigned char *src, int sz, unsigned char *dest);
+  #extern void mdb_free_indices(GPtrArray *indices);
+  #void mdb_index_page_reset(MdbIndexPage *ipg);
+  #extern int mdb_index_pack_bitmap(MdbHandle *mdb, MdbIndexPage *ipg);
+
+  #/* stats.c */
+  #extern void mdb_stats_on(MdbHandle *mdb);
+  #extern void mdb_stats_off(MdbHandle *mdb);
+  #extern void mdb_dump_stats(MdbHandle *mdb);
+
+  #/* like.c */
+  #extern int mdb_like_cmp(char *s, char *r);
+
+  #/* write.c */
+  #extern void mdb_put_int16(void *buf, guint32 offset, guint32 value);
+  #extern void mdb_put_int32(void *buf, guint32 offset, guint32 value);
+  #extern void mdb_put_int32_msb(void *buf, guint32 offset, guint32 value);
+  #extern int mdb_crack_row(MdbTableDef *table, int row_start, int row_end, MdbField *fields);
+  #extern guint16 mdb_add_row_to_pg(MdbTableDef *table, unsigned char *row_buffer, int new_row_size);
+  #extern int mdb_update_index(MdbTableDef *table, MdbIndex *idx, unsigned int num_fields, MdbField *fields, guint32 pgnum, guint16 rownum);
+  #extern int mdb_insert_row(MdbTableDef *table, int num_fields, MdbField *fields);
+  #extern int mdb_pack_row(MdbTableDef *table, unsigned char *row_buffer, unsigned int num_fields, MdbField *fields);
+  #extern int mdb_replace_row(MdbTableDef *table, int row, void *new_row, int new_row_size);
+  #extern int mdb_pg_get_freespace(MdbHandle *mdb);
+  #extern int mdb_update_row(MdbTableDef *table);
+  #extern void *mdb_new_data_pg(MdbCatalogEntry *entry);
+
+  #/* map.c */
+  #extern guint32 mdb_map_find_next_freepage(MdbTableDef *table, int row_size);
+  #extern gint32 mdb_map_find_next(MdbHandle *mdb, unsigned char *map, unsigned int map_sz, guint32 start_pg);
+
+  #/* props.c */
+  #extern void mdb_free_props(MdbProperties *props);
+  #extern void mdb_dump_props(MdbProperties *props, FILE *outfile, int show_name);
+  #extern GArray* mdb_kkd_to_props(MdbHandle *mdb, void *kkd, size_t len);
+
+  #/* worktable.c */
+  #extern MdbTableDef *mdb_create_temp_table(MdbHandle *mdb, char *name);
+  #extern void mdb_temp_table_add_col(MdbTableDef *table, MdbColumn *col);
+  #extern void mdb_fill_temp_col(MdbColumn *tcol, char *col_name, int col_size, int col_type, int is_fixed);
+  #extern void mdb_fill_temp_field(MdbField *field, void *value, int siz, int is_fixed, int is_null, int start, int column);
+  #extern void mdb_temp_columns_end(MdbTableDef *table);
+
+  ## options.c 
+  #extern int mdb_get_option(unsigned long optnum);
+  #extern void mdb_debug(int klass, char *fmt, ...);
+
+  # iconv
+  #extern int mdb_unicode2ascii(MdbHandle *mdb, char *src, size_t slen, char *dest, size_t dlen);
+  #extern int mdb_ascii2unicode(MdbHandle *mdb, char *src, size_t slen, char *dest, size_t dlen);
+  #extern void mdb_iconv_init(MdbHandle *mdb);
+  #extern void mdb_iconv_close(MdbHandle *mdb);
+  #extern const char* mdb_target_charset(MdbHandle *mdb);
 end
